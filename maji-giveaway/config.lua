eb99d720ef493b2c9013fa34cca4c5c3e525f5a3
Maji <joey.francies@gmail.com>
2023-09-17 09:36:57 -0500

    Update config.lua

--- maji-giveaway/config.lua
+++ maji-giveaway/config.lua
@@ -1,6 +1,3 @@
 Config = Config or {}
 
 Config.GiveawayNumber = 100 -- Player count for your giveaway. So when the 100th person joins, it will post a webhook stating that the 100th player has joined, and will tag them.
-
-Config.Webhook = "YOUR_WEBHOOK_HERE" -- create a webhook and place it here
-
